bf8352c1bd5ab864abf3ce92de0f9f31b5fda77e
DavidDevGt <josuedavidvl18@gmail.com>
2023-10-11 08:04:10 -0600

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <BlockGame />
+</template>
+
+<script>
+import BlockGame from './components/BlockGame.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BlockGame
+  }
+}
+</script>
+
+<style></style>
